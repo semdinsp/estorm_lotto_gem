15d3c2f62073962894298ec6c1c183097f97f27e
semdinsp <scott.sproule@ficonab.com>
2014-02-19 11:05:37 +0800

    updates to pipp calls

--- lib/estorm_lotto_gem/button.rb
+++ lib/estorm_lotto_gem/button.rb
@@ -2,7 +2,7 @@ require 'pi_piper'
 module EstormLottoGem
   class Button
     include PiPiper
-    attr_accessor :led, :pin
+    attr_accessor :led, :pin, :t
     def tap
     self.led.on
     puts "button TAPPED"
@@ -25,18 +25,15 @@ module EstormLottoGem
     def manage_buttons
       self.pin=PiPiper::Pin.new(:pin => 23, :pull => :up)
       self.led=PiPiper::Pin.new(:pin => 18, :direction => :out)
-      t=Time.now
-      PiPiper.watch :pin => 23,:trigger => :falling , :pull => :up do
+      @t=Time.now
+      self.watch :pin => 23,:trigger => :falling , :pull => :up do
         #puts "Button pressed changed from #{last_value} to #{value}"
         #puts "."
-        t=Time.now
+        @t=Time.now
       end
-      PiPiper.watch :pin => 23,:trigger => :rising , :pull => :up do
-        delta = Time.now.to_f - t.to_f
-        t=Time.now
-        #puts "t is #{t} time is #{Time.now} delta is #{delta.inspect} usec "
-        #puts "delta is #{delta}"
-        #puts "Button released: changed from #{last_value} to #{value} delta: #{delta}"
+      self.watch :pin => 23,:trigger => :rising , :pull => :up do
+        delta = Time.now.to_f - @t.to_f
+        #@t = Time.now
         self.tap() if 0.03 <= delta and delta < 0.7
         self.held()  if 2 < delta and  delta < 20
         #puts "debounce" if 0.1 > delta
@@ -44,8 +41,7 @@ module EstormLottoGem
       sleep 5
       bootup
       self.led.off
-      t=Time.now
-      PiPiper.wait
+      self.wait
     end
   end
 end  # module
