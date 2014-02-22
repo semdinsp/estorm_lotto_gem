a5538a67e6a90f64e8b993de8bc86704745a456f
semdinsp <scott.sproule@ficonab.com>
2014-02-22 22:14:43 +0800

    added lotto ticket stuff

--- bin/test_cli.rb
+++ bin/test_cli.rb
@@ -15,6 +15,15 @@ class TestCli < Thor
       res=wb.get_balance(options[:source])
       res
     end
+    option :source, :required => true
+    option :host, :required => true
+     option :message, :required => false
+    def get_lotto
+      wb=EstormLottoGem::WbLotto4d.new
+      wb.set_host(options[:host])
+      res=wb.get_ticket(options[:source],options[:message])
+      res
+    end
     
     
 end
