4b2089da2d8297019f6c350c589773f341ae369b
semdinsp <scott.sproule@ficonab.com>
2014-02-23 12:23:27 +0800

    removed code climate settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ puts "in test helper"
 require 'rubygems'
 require 'bundler/setup'
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start 
+#CodeClimate::TestReporter.start 
 require 'stringio'
 require 'minitest/autorun'
 require 'minitest/unit'
