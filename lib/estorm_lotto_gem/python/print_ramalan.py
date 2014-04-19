3ad1ab49bd597598452669b0ce47a6af4a1fcf36
semdinsp <scott.sproule@ficonab.com>
2014-04-19 16:56:37 +0800

    shio list updat for ramalan

--- lib/estorm_lotto_gem/python/print_ramalan.py
+++ lib/estorm_lotto_gem/python/print_ramalan.py
@@ -11,12 +11,14 @@ printer_type=sys.argv[5]
 seller=sys.argv[6]
 drawtype=sys.argv[7]
 rama=sys.argv[8]
-
+shiolist="Naga 01,12,25,37,49,61,73,85\nKelinci 02,14,26,38,50,62,74,86\nMacan 03,15,27,39,51,63,75,87,99\nSapi 04,16,28,40,52,64,6,88,00"
+shiolist2="Tikus 05,17,29,41,53,65,77,89\nBai 06,18,30,42,54,66,78,90\nAnjing 07,19,31,43,55,67,79,91\nAyam 08,20,32,44,56,68,80,92"
+shiolist3="Monyet 09,21,33,45,57,69,81,93\nKambing 10,22,34,46,58,70,82,94\nKuda 11,23,35,47,59,71,83,95\nUlar 12,24,36,48,60,72,84,96"
 
 pos_printer=Teds_Printer(printer_type)
 pos_printer.large()
 #ada_printer.printImage(Image.open('/home/pi/Python-Thermal-Printer/gfx/luckysms.png'), True)
-pos_printer.println("Lucky Loja Ramalan")
+pos_printer.println("Lucky Loja Ramalan 2014 Year of the Horse")
 pos_printer.println(drawtype)
 pos_printer.normal()
 pos_printer.space()
@@ -41,5 +43,11 @@ pos_printer.println(kapala)
 pos_printer.normal()
 pos_printer.println("Sold by:\n")
 pos_printer.println(seller)
+pos_printer.println("Shiolist")
+pos_printer.println(shiolist)
+pos_printer.println(shiolist1)
+pos_printer.println(shiolist2)
+pos_printer.println(shiolist3)
+
 # pos_printer.printBarcode(sec_code, ada_printer.CODE128)
 pos_printer.closing()
