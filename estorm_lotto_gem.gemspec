cdf8770b35162144b80abdf95d6c3b518bb11e1c
semdinsp <scott.sproule@ficonab.com>
2014-06-08 09:16:28 +0800

    update to sports instantt win

--- estorm_lotto_gem.gemspec
+++ estorm_lotto_gem.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "estorm_lotto_gem"
-  s.version     = "1.4.0"
+  s.version     = "1.4.1"
   s.author      = "Scott Sproule"
   s.email       = "scott.sproule@ficonab.com"
   s.homepage    = "http://github.com/semdinsp/estorm_lotto_gem"
--- lib/estorm_lotto_gem/wb_sports.rb
+++ lib/estorm_lotto_gem/wb_sports.rb
@@ -8,7 +8,12 @@ module EstormLottoGem
       res
     end
     def self.teams
-      [["Spain","Spain"], ["Barcelona","Barcelona"],["Croatia","Croatia"],["Brasil","Brasil"]]
+      [["Brazil","Brazil"],["Croatia","Croatia"],["Mexico","Mexico"],["Cameroon","Cameroon"],["Spain","Spain"],
+      ["Netherlands","Netherlands"],["Chile","Chile"],["Australia","Australia"],["Greece","Greece"],["Japan","Japan"],
+      ["Cote D'Ivoire","Cote D'Ivoire"],["Uraguay","Uraguay"],["Costa Rica","Costa Rica"],["England","England"],["Italy","Italy"],
+      ["Switzerland","Switzerland"],["Ecuador","Ecuador"],["France","France"],["Honduras","Honduras"],["Argentina","Argentina"],["Iran","Iran"],["Bosnia-Herzagovina","Bosnia-Herzagovina"],["Nigeria","Nigeria"],
+      ["Germany","Germany"],["Portugal","Portugal"],["Ghana","Ghana"],["USA","USA"],["Beligium","Beligium"],
+      ["Algeria","Algeria"],["Russia","Russia"],["Korea","Korea"],["Brazil","Brazil"]]
     end
     def print_instant_first(seller,team,txid,printer_type='adafruit')
        teams=EstormLottoGem::WbSports.teams
@@ -22,6 +27,7 @@ module EstormLottoGem
        msgs=[msg0,msg1,msg2,msg3].join("\n").to_s
         
        system("/usr/bin/python","#{self.python_directory}/print_instant1.py",team,txid,seller,msgs,printer_type) if printer_type!= "none"
+       @oldmsg=msgs
        [msgs]
     end
     def print_results(res,seller,team,txid,printer_type='adafruit')
@@ -38,7 +44,7 @@ module EstormLottoGem
         end
         #system("/usr/bin/python","/home/pi/Python-Thermal-Printer/print_ticket.py",digits,drawdate,code,exmsgs,printer_type) if printer_type!= "none"
        system("/usr/bin/python","#{self.python_directory}/print_sports.py",msg,seller,team,txid,printer_type,res['prize'].to_s ) if printer_type!= "none"
-       respstring="#{winner} #{team} versus #{@opponents} #{msg} prize #{res['prize']}"
+       respstring="#{@oldmsg}\n#{winner} #{team} versus #{@opponents} #{msg} prize #{res['prize']}".gsub("\n","</p></p>")
        [respstring]
     end
   end # clase
