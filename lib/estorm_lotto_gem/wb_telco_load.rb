02310b56794ff5e6e4af82a444c9c9e96df91b0c
semdinsp <scott.sproule@ficonab.com>
2014-12-15 14:44:28 +0800

    update fro working pulsa sales

--- lib/estorm_lotto_gem/wb_telco_load.rb
+++ lib/estorm_lotto_gem/wb_telco_load.rb
@@ -16,11 +16,12 @@ module EstormLottoGem
        serial=res.first['serial'] if res.first!=nil
        telco=res.first['telco'] if res.first!=nil
        txid=res[1]['txid'] if res[1]!=nil
+       cost=res[1]['transaction_fee'] if res[1]!=nil
        msg="thank you from Telkcomcel"
        puts  "print telco load#{res} class #{res.class}"
          system("/usr/bin/python","#{self.python_directory}/print_telco_load.py",
                 pin,seller,value.to_s,printer_type,msg,txid,telco,serial) if printer_type!= "none"    
-       respstring="Sold #{value} Telco: #{telco}\nPin #{pin}\ntxid: #{txid} serial #{serial}".gsub("\n","</p></p>")
+       respstring="Sold #{value} Telco: #{telco}\nPin #{pin}\ntxid: #{txid} serial #{serial} cost: #{cost}".gsub("\n","</p></p>")
        [respstring]
         
     end
