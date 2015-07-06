26880f6d9ee2f8cf1044b35e0682336af6c21f50
semdinsp <scott.sproule@ficonab.com>
2015-07-06 14:37:01 +0800

    now supports few other vendors

--- lib/estorm_lotto_gem/constants.rb
+++ lib/estorm_lotto_gem/constants.rb
@@ -18,7 +18,7 @@ module EstormLottoGem
      
   end
   def self.telcos
-    build_array(['telkomcel','scott','bwdi'])
+    build_array(['telkomcel','etdl','tt','telemor','scott','bwdi'])
     
   end
   def self.pulsa_values
