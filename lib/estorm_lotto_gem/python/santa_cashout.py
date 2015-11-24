ad5c367a4b0e6a7888a54d7c881ce33102c50dbe
semdinsp <scott.sproule@ficonab.com>
2015-11-24 20:01:37 +0800

    new version including print file for snta

---
+++ lib/estorm_lotto_gem/python/santa_cashout.py
@@ -0,0 +1,23 @@
+from Adafruit_Thermal import *
+from Teds_Printer import *
+import Image, sys
+from datetime import datetime
+from escpos import *
+winner=sys.argv[1]
+prize=sys.argv[2]
+printer_type=sys.argv[3]
+txid=sys.argv[4]
+prizetype=sys.argv[5]
+label=sys.argv[6]
+pos_printer=Teds_Printer(printer_type)
+pos_printer.large()
+pos_printer.println("Santa Cashout Ticket")
+pos_printer.println(label)
+pos_printer.normal()
+pos_printer.println("Prize Value")
+pos_printer.println(prize)
+pos_printer.println(prizetype)
+pos_printer.println("Winning Customer")
+pos_printer.println(winner)
+pos_printer.txid_code(txid)
+pos_printer.end_ticket("Redeemed by:",seller)
