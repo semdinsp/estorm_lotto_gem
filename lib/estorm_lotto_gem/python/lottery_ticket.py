6a295d73a980f1fdb62ce5d4bdd659d67a9654a4
semdinsp <scott.sproule@ficonab.com>
2017-11-28 16:42:26 +0800

    update for printing med code

--- lib/estorm_lotto_gem/python/lottery_ticket.py
+++ lib/estorm_lotto_gem/python/lottery_ticket.py
@@ -34,7 +34,7 @@ tms_printer.normal()
 tms_printer.println("Ticket Count: " + str(options['ticket_count']))
 tms_printer.println("Ticket Price: " + str(options['txfee']))
 tms_printer.space()
-tms_printer.lotto_qr_code(options['qrcode'],"Ticket/Security Codes")
+tms_printer.lotto_qr_code(options['qrcode'],str(options['md5code']))
 tms_printer.tms_end_ticket("Processed by:",seller)
 
 
