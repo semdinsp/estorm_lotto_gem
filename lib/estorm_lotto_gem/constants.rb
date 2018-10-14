dc81457154a45c599d4036a610a235db301cfb43
semdinsp <scott.sproule@ficonab.com>
2018-10-14 18:46:08 +0800

    update for contacts and gemspec

--- lib/estorm_lotto_gem/constants.rb
+++ lib/estorm_lotto_gem/constants.rb
@@ -90,7 +90,7 @@ module EstormLottoGem
   end
   
   def self.district_manager_teams
-    build_array(['unassigned','None','Cancio','Zeferino','Hermenio','Mgmt'])
+    build_array(['unassigned','None','Cancio','Zeferino','Hermenio','Mgmt','scott'])
   end
   
   def self.report_types
